254187d205ba32b3ddc0c495b1183abf8a8d924e
Dan34523 <danyal.durrani123@gmail.com>
2020-09-30 14:50:18 +0100

    Add some current client source code and update project settings
    Added as much of the current source code from the first project to the client source code. Boost libraries hadn't been properly committed so redid that as well.

---
+++ BoostAsyncClientServer/BoostClient/Client.cpp
@@ -0,0 +1,82 @@
+#include "Client.h"
+
+std::mutex mu;
+bool terminateProgram = false;
+
+struct dataStruct {
+	string data;
+	bool ready;
+};
+
+queue<dataStruct> clientSendQueue;
+queue<dataStruct> clientRecvQueue;
+
+string read_(tcp::socket& sock, streambuf& buf, boost::system::error_code& err) {
+
+	char buffer[512];
+	const size_t len = sock.read_some(boost::asio::buffer(buffer), err);
+
+	/*	socket.read_some changes the buffer to an array but doesn't add the null terminating
+		character. Line 21 adds it on so it can be correctly used with iostream and string objects */
+	buffer[len] = '\0';
+
+	if (err && err != error::eof) {
+		cerr << err.message() << endl;
+		return "-1";
+	}
+
+	else {
+		string data = buffer_cast<const char*> (buf.data());
+		return buffer;
+	}
+}
+
+void recvThread(tcp::socket& sock, streambuf& buf, boost::system::error_code& err, string const& username) {
+	while (!terminateProgram) {
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		const string serverMsg = read_(sock, buf, err);
+
+		if (serverMsg == "-1") {
+			break;
+		}
+
+		dataStruct serverOut;
+		serverOut.data = serverMsg;
+		serverOut.ready = true;
+
+		clientRecvQueue.push(serverOut);
+
+		if (serverMsg.substr(0, serverMsg.find(" ")) == "002" + username) {
+			cout << "Received Termination Message. Quitting..." << endl;
+			terminateProgram = true;
+		}
+	}
+}
+
+void sendThread(tcp::socket& sock, boost::system::error_code& ec, string const& username) {
+	while (!terminateProgram) {
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+		while (!clientSendQueue.empty()) {
+
+			const dataStruct front = clientSendQueue.front();
+
+			if (front.ready) {
+
+				write(sock, buffer(username + " " + front.data), ec);
+				
+				if (!ec) {
+					clientSendQueue.pop();
+				}
+
+				else {
+					cout << "Send failed. Error Message: " << ec.message() << endl;
+				}
+
+			}
+
+		}
+
+	}
+}
--- BoostAsyncClientServer/BoostClient/Client.h
+++ BoostAsyncClientServer/BoostClient/Client.h
@@ -4,3 +4,26 @@
 #include <boost/asio.hpp>
 #include <string>
 #include <queue>
+#include <mutex>
+#include <chrono>
+
+#ifdef _WIN32 // Adds necessary headers for Sleep function
+#include <Windows.h> // If on a Windows OS
+#else
+#include <unistd.h> // If on a POSIX/Linux derived OS
+#endif
+
+using namespace boost::asio;
+
+// Using tags for iostream
+using std::cerr;
+using std::endl;
+using std::cout;
+using std::cin;
+using std::getline;
+
+// Using tags for other libraries
+using std::string;
+using std::queue;
+
+using ip::tcp;
